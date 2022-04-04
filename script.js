c22876a07c4fc6226af6d582a8fec5f9ce897230
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-04-03 22:57:52 -0300

    Colocado paletas de cores

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+corClass = 'color-Amarelo';
+
 document.querySelector('.post-it').addEventListener('dragstart', dragstart);
 document.querySelector('.post-it').addEventListener('dragend', dragend);
+
+document.querySelectorAll('.cores').forEach(cores =>{
+    cores.addEventListener('click', (item)=>{
+
+        elemento = document.querySelector('.blocks textarea');
+        cor = item.target.classList[0];
+        corClass = cor;
+        console.log(cor);
+        limpar();
+        elemento.classList.add(cor);        
+    });
+});
 
 document.querySelectorAll('.area').forEach(area =>{
     area.addEventListener('dragover', dragover);
@@ -27,17 +41,37 @@ function dragleave(e){
 }
 
 function drop(e){
+        elemento = document.querySelector('.blocks textarea');
+        console.log(elemento);        
         let x = document.querySelector('.dragging');
+        console.log(x);
         e.currentTarget.appendChild(x);
         e.currentTarget.classList.remove('hover');
         testar();
 }
 
 function testar(){
-    let text = document.querySelector('.blocks').innerHTML = '<textarea name="" class="post-it" cols="30" rows="10" draggable="true"></textarea>';
+    let text = document.querySelector('.blocks').innerHTML += `<textarea name="" class="post-it ${corClass}" cols="30" rows="10" draggable="true"></textarea>`;
     document.querySelectorAll('.post-it').forEach(postado =>{
         postado.addEventListener('dragstart', dragstart);
         postado.addEventListener('dragend', dragend);
     });
+    document.querySelectorAll('.cores').forEach(cores =>{
+        cores.addEventListener('click', (item)=>{
     
+            elemento = document.querySelector('.blocks textarea');
+            cor = item.target.classList[0];
+            corClass = cor;
+            console.log(cor);
+            limpar();
+            elemento.classList.add(cor);        
+        });
+    });
+}
+
+function limpar(){
+    document.querySelector('.blocks textarea').classList.remove('color-azul');
+    document.querySelector('.blocks textarea').classList.remove('color-Amarelo');
+    document.querySelector('.blocks textarea').classList.remove('color-verde');
+    document.querySelector('.blocks textarea').classList.remove('color-rosa');
 }
